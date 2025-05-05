814de0efaeaf61f29e6013e672a85458b355e48f
marcobolanosfelix <marcobf1996@gmail.com>
2025-05-04 22:33:22 -0700

    Implementing insert and show data in the simple linked list

---
+++ src/ListApplication.java
@@ -0,0 +1,15 @@
+import simple_linked_list.SimpleLinkedList;
+
+public class ListApplication {
+
+    public static void main(String[] args) {
+        
+        SimpleLinkedList<Integer> simpleLinkedList = new SimpleLinkedList<>();
+        simpleLinkedList.addNode(8);
+        simpleLinkedList.addNode(5);
+        simpleLinkedList.addNode(6);
+        simpleLinkedList.showList();
+
+    }
+
+}
